5cb3504b7fe21db40ec9638aed4ec897a7bc9bf1
majidzahedi <eemajidzahedi@hotmail.com>
2022-04-18 18:26:51 +0430

    Fix CORS modified

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-from fastapi import Depends, FastAPI
-# from .import models
-# from .database import engin
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 
-# models.Base.metadata.create_all(bind=engin)
-
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
