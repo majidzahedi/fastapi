c62c2472c010e4b6f091be5699c7b76da8df1094
majidzahedi <eemajidzahedi@hotmail.com>
2022-04-18 12:09:01 +0430

    Feat User & Post relatoinsip added

--- app/routers/post.py
+++ app/routers/post.py
@@ -16,6 +16,13 @@ def posts(db: Session = Depends(get_db)):
     return posts
 
 
+@router.get('/profile', response_model=List[schema.Post])
+def user_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
+    posts = db.query(models.Post).filter(
+        models.Post.owner_id == current_user.id).all()
+    return posts
+
+
 @router.get('/{id}', response_model=schema.Post)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -29,7 +36,7 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schema.Post)
 def create_post(post: schema.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -39,25 +46,35 @@ def create_post(post: schema.PostCreate, db: Session = Depends(get_db), current_
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, response_model=schema.Post)
 def update_post(id: int, updated_post: schema.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
 
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if not post.first():
+    if not post:
         raise HTTPException(
             detail=f'post with {id} not found', status_code=status.HTTP_404_NOT_FOUND)
 
-    post.update(updated_post.dict())
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Not Authorized to perform this action.')
+
+    post_query.update(updated_post.dict())
     db.commit()
-    return post.first()
+    return post
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if not post.first():
+    if not post:
         raise HTTPException(
             detail=f'post with {id} not found', status_code=status.HTTP_404_NOT_FOUND)
 
-    post.delete()
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Not Authorized to perform this action.')
+
+    post_query.delete()
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
